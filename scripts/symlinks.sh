e9135d90047cf7f4c241b4b90f863edb08662b13
Reiler222 <reiler222@gmail.com>
2023-09-04 20:54:26 +0200

    Damn symlinks

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd ..
+workingDir=$(pwd) 
 
 if [ -f "$HOME/.bashrc" ]; then
     rm -f "$HOME/.bashrc"
 fi
 
-ln -s ../.bashrc $HOME/.bashrc 
-ln -s ../.zshrc $HOME/.zshrc  
-ln -s ../.gitconfig $HOME/.gitconfig  
-ln -s ../starship.toml $HOME/.config/starship.toml
+ln -s ${dotfilesDir}/.bashrc $HOME/.bashrc 
+ln -s ${dotfilesDir}/.zshrc $HOME/.zshrc  
+ln -s ${dotfilesDir}/.gitconfig $HOME/.gitconfig  
+ln -s ${dotfilesDir}/starship.toml $HOME/.config/starship.toml
