6d34c15ce184849020a00da59e823c8db747f57e
Reiler222 <reiler222@gmail.com>
2023-09-05 20:22:49 +0200

    Another other fix in the wall

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -8,7 +8,7 @@ if [ -f "$HOME/.bashrc" ]; then
     rm -f "$HOME/.bashrc"
 fi
 
-ln -s $(workingDir)/.bashrc $HOME/.bashrc 
-ln -s $(workingDir)/.zshrc $HOME/.zshrc  
-ln -s $(workingDir)/.gitconfig $HOME/.gitconfig  
-ln -s $(workingDir)/starship.toml $HOME/.config/starship.toml
+ln -s $workingDir/.bashrc $HOME/.bashrc 
+ln -s $workingDir/.zshrc $HOME/.zshrc  
+ln -s $workingDir/.gitconfig $HOME/.gitconfig  
+ln -s $workingDir/starship.toml $HOME/.config/starship.toml
