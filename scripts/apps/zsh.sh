c805827dd36f9753f87f2cb5654e905ef04abdb9
Reiler222 <reiler222@gmail.com>
2023-09-10 19:57:41 +0200

    Fix zsh install

--- scripts/apps/zsh.sh
+++ scripts/apps/zsh.sh
@@ -2,16 +2,22 @@
 
 echo "Instalando zsh y plugins..."
 
+mkdir $HOME/.zsh
+
 sudo apt install -y zsh
-sudo apt install -y vivid
+
+wget "https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb"
+sudo dpkg -i vivid_0.8.0_amd64.deb
 
 # Installing plugins for zsh
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
 
-git clone https://github.com/z-shell/F-Sy-H ~/.zsh/fast-syntax-highlight
+git clone https://github.com/z-shell/F-Sy-H $HOME/.zsh/fast-syntax-highlight
 
 # Autocompletion plugin
 
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/completion.zsh
-mv completion.zsh ~/.zsh/
+mv completion.zsh $HOME/.zsh/
+
+rm -f vivid_0.8.0_amd64.deb
